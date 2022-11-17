dec9a14ce8ce3e55c6c4360926fa51474729d538
David Zhou <wi21b026@technikum-wien.at>
2022-11-17 14:12:08 +0100

    added pictures to index

--- index.php
+++ index.php
@@ -27,15 +27,14 @@
 
     <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" aria-label="Slide 1" class="active" aria-current="true"></button>
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="" ></button>
       </div>
       <div class="carousel-inner">
         <div class="carousel-item">
-          <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-            <rect width="100%" height="100%" fill="#777"></rect>
-          </svg>
+          <img src="./images/Hotel/hotel_aussen.jpg" alt="Hotel Foto von aussen" width="100%" height="100%">
+    
 
           <div class="container">
             <div class="carousel-caption text-start">
