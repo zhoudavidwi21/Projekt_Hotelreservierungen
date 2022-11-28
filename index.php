6fde452061cd0524d7436972bad56b3823d39bea
Martin <martin@frischmann.ch>
2022-11-28 03:14:19 +0100

    Änderung Timing carousel

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
 
         <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-bs-ride="carousel">
           <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="2000">
+            <div class="carousel-item active" data-bs-interval="3000">
               <img src="Images/Zimmer/Zimmer_01_600x400.jpg" class="d-block w-100" alt="">
             </div>
             <div class="carousel-item" data-bs-interval="2000">
