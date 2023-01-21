7ede7ecbb0beb6c640b0693b71dd016209f2322d
David <david281100@hotmail.com>
2023-01-21 16:38:38 +0100

    formatted zimmer_reservieren table

--- zimmer_reservieren_confirmed.php
+++ zimmer_reservieren_confirmed.php
@@ -27,153 +27,157 @@ if ($db_obj->connect_error) {
 
             <h1 class="h3 mb-3 fw-normal">Reservierung bestätigen</h1>
 
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Beschreibung</th>
-                        <th scope="col">Preis</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">Zimmer Nummer</th>
-                        <td>
-                            <?php if (isset($_SESSION['resRoom'])) {
-                                echo $_SESSION['resRoom'];
-                            } ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Anreisedatum</th>
-                        <td>
-                            <?php if (isset($_SESSION['resArrival'])) {
-                                $date1 = date_create($_SESSION['resArrival']);
-                                echo date_format($date1, "d.m.Y");
-                            } ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Abreisedatum</th>
-                        <td>
-                            <?php if (isset($_SESSION['resDeparture'])) {
-                                $date2 = date_create($_SESSION['resDeparture']);
-                                echo date_format($date2, "d.m.Y");
-                            } ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Anzahl der Nächte</th>
-                        <td>
-                            <?php if (isset($_SESSION['resDeparture'])) {
-                                $diff = date_diff($date1, $date2, true);
-                                echo $diff->format("%a");
+            <div class="table-responsive">
+                <div class="table-wrapper">
 
-                            } ?>
-                        </td>
-                        <td>
-                            <?php
-                            $query = "SELECT * FROM rooms WHERE roomNumber = '$_SESSION[resRoom]'";
-                            $result = $db_obj->query($query);
-                            $row = $result->fetch_assoc();
-                            $roomPrice = floatval($row['roomPrice']);
-                            echo number_format($roomPrice * $diff->format("%a"), 2, ",", ".") . "€";
-                            ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Frühstück</th>
-                        <td>
-                            <?php if (isset($_SESSION['resBreakfast'])) {
-                                if ($_SESSION['resBreakfast'] == "true") {
-                                    echo "Ja";
-                                } else {
-                                    echo "Nein";
-                                }
-                            } ?>
-                        </td>
-                        <td>
-                            <?php
-                            $query = "SELECT * FROM services WHERE serviceName = 'breakfast'";
-                            $result = $db_obj->query($query);
-                            $row = $result->fetch_assoc();
-                            $breakfastPrice = floatval($row['servicePrice']);
-                            echo number_format($breakfastPrice * $diff->format("%a"), 2, ",", ".") . "€";
-                            ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Parkplatz</th>
-                        <td>
-                            <?php if (isset($_SESSION['resParking'])) {
-                                if ($_SESSION['resParking'] == "true") {
-                                    echo "Ja";
-                                } else {
-                                    echo "Nein";
-                                }
-                            } ?>
-                        </td>
-                        <td>
-                            <?php
-                            $query = "SELECT * FROM services WHERE serviceName = 'parking'";
-                            $result = $db_obj->query($query);
-                            $row = $result->fetch_assoc();
-                            $parkingPrice = floatval($row['servicePrice']);
-                            echo number_format($parkingPrice * $diff->format("%a"), 2, ",", ".") . "€";
-                            ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Haustier</th>
-                        <td>
-                            <?php if (isset($_SESSION['resPet'])) {
-                                if ($_SESSION['resPet'] == "true") {
-                                    echo "Ja";
-                                } else {
-                                    echo "Nein";
-                                }
-                            } ?>
-                        </td>
-                        <td>
-                            <?php
-                            $query = "SELECT * FROM services WHERE serviceName = 'pet'";
-                            $result = $db_obj->query($query);
-                            $row = $result->fetch_assoc();
-                            $parkingPrice = floatval($row['servicePrice']);
-                            echo number_format($parkingPrice * $diff->format("%a"), 2, ",", ".") . "€";
-                            ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Gesamtpreis</th>
-                        <td colspan="2">
-                            <?php
-                            $roomTotal = $roomPrice * $diff->format("%a");
-                            $serviceTotal = 0;
-                            if (isset($_SESSION['resBreakfast']) && $_SESSION['resBreakfast'] == "true") {
-                                $serviceTotal += $breakfastPrice * $diff->format("%a");
-                            }
-                            if (isset($_SESSION['resParking']) && $_SESSION['resParking'] == "true") {
-                                $serviceTotal += $parkingPrice * $diff->format("%a");
-                            }
-                            $sum = $roomTotal + $serviceTotal;
-                            echo '<p class=fw-bold>' . number_format($sum, 2, ",", ".") . "€" . '</p>';
-                            $_SESSION['resTotal'] = $sum;
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Beschreibung</th>
+                                <th scope="col">Preis</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Zimmer Nummer</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resRoom'])) {
+                                        echo $_SESSION['resRoom'];
+                                    } ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Anreisedatum</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resArrival'])) {
+                                        $date1 = date_create($_SESSION['resArrival']);
+                                        echo date_format($date1, "d.m.Y");
+                                    } ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Abreisedatum</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resDeparture'])) {
+                                        $date2 = date_create($_SESSION['resDeparture']);
+                                        echo date_format($date2, "d.m.Y");
+                                    } ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Anzahl der Nächte</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resDeparture'])) {
+                                        $diff = date_diff($date1, $date2, true);
+                                        echo $diff->format("%a");
 
-                            $result->free();
-                            $db_obj->close();
-                            ?>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                                    } ?>
+                                </td>
+                                <td>
+                                    <?php
+                                    $query = "SELECT * FROM rooms WHERE roomNumber = '$_SESSION[resRoom]'";
+                                    $result = $db_obj->query($query);
+                                    $row = $result->fetch_assoc();
+                                    $roomPrice = floatval($row['roomPrice']);
+                                    echo number_format($roomPrice * $diff->format("%a"), 2, ",", ".") . "€";
+                                    ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Frühstück</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resBreakfast'])) {
+                                        if ($_SESSION['resBreakfast'] == "true") {
+                                            echo "Ja";
+                                        } else {
+                                            echo "Nein";
+                                        }
+                                    } ?>
+                                </td>
+                                <td>
+                                    <?php
+                                    $query = "SELECT * FROM services WHERE serviceName = 'breakfast'";
+                                    $result = $db_obj->query($query);
+                                    $row = $result->fetch_assoc();
+                                    $breakfastPrice = floatval($row['servicePrice']);
+                                    echo number_format($breakfastPrice * $diff->format("%a"), 2, ",", ".") . "€";
+                                    ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Parkplatz</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resParking'])) {
+                                        if ($_SESSION['resParking'] == "true") {
+                                            echo "Ja";
+                                        } else {
+                                            echo "Nein";
+                                        }
+                                    } ?>
+                                </td>
+                                <td>
+                                    <?php
+                                    $query = "SELECT * FROM services WHERE serviceName = 'parking'";
+                                    $result = $db_obj->query($query);
+                                    $row = $result->fetch_assoc();
+                                    $parkingPrice = floatval($row['servicePrice']);
+                                    echo number_format($parkingPrice * $diff->format("%a"), 2, ",", ".") . "€";
+                                    ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Haustier</th>
+                                <td>
+                                    <?php if (isset($_SESSION['resPet'])) {
+                                        if ($_SESSION['resPet'] == "true") {
+                                            echo "Ja";
+                                        } else {
+                                            echo "Nein";
+                                        }
+                                    } ?>
+                                </td>
+                                <td>
+                                    <?php
+                                    $query = "SELECT * FROM services WHERE serviceName = 'pet'";
+                                    $result = $db_obj->query($query);
+                                    $row = $result->fetch_assoc();
+                                    $parkingPrice = floatval($row['servicePrice']);
+                                    echo number_format($parkingPrice * $diff->format("%a"), 2, ",", ".") . "€";
+                                    ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Gesamtpreis</th>
+                                <td colspan="2">
+                                    <?php
+                                    $roomTotal = $roomPrice * $diff->format("%a");
+                                    $serviceTotal = 0;
+                                    if (isset($_SESSION['resBreakfast']) && $_SESSION['resBreakfast'] == "true") {
+                                        $serviceTotal += $breakfastPrice * $diff->format("%a");
+                                    }
+                                    if (isset($_SESSION['resParking']) && $_SESSION['resParking'] == "true") {
+                                        $serviceTotal += $parkingPrice * $diff->format("%a");
+                                    }
+                                    $sum = $roomTotal + $serviceTotal;
+                                    echo '<p class=fw-bold>' . number_format($sum, 2, ",", ".") . "€" . '</p>';
+                                    $_SESSION['resTotal'] = $sum;
 
-            <a href="index.php?site=zimmer_reservieren" class="btn btn-secondary">Zurück</a>
+                                    $result->free();
+                                    $db_obj->close();
+                                    ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-            <a href="reservierung_bestaetigen.php" class="btn btn-sonstige">
-                Bestätigen
-            </a>
+                <a href="index.php?site=zimmer_reservieren" class="btn btn-secondary">Zurück</a>
 
+                <a href="reservierung_bestaetigen.php" class="btn btn-sonstige">
+                    Bestätigen
+                </a>
+            </div>
         </div>
     </div>
 </div>
