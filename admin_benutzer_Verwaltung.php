6ac5b55b7b97d1f0ea5b7471d7458622077eef47
David <david281100@hotmail.com>
2023-01-23 13:00:38 +0100

    fixed sites

--- admin_benutzer_Verwaltung.php
+++ admin_benutzer_Verwaltung.php
@@ -5,7 +5,7 @@
 <?php
 //Nur Admins können Benutzer verwalten
 if (isset($_SESSION['role']) && $_SESSION['role'] !== "admin") {
-  header('Refresh:1; url=index.php?error');
+  header('Refresh:1; url=index.php?site=error');
   exit();
 }
 ?>
